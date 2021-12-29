eef1da8c6531466dcbf7c94b7ecc3a3f5f963baf
gtasa3box <gtasa3box@gmail.com>
2021-12-29 16:18:17 +0300

    com

--- src/js/global.js
+++ src/js/global.js
@@ -42,3 +42,39 @@ $('.slider-card__slider').slick({
       }
     ]
   });
+
+//news slider
+$('.news-popup__slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+//load slider popup
+$('.news-popup').slick('setPosition'); 
